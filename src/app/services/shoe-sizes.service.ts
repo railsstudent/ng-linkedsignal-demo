158110cbb71004c9428ac8aac72b26eec98b1edb
Connie Leung <cleung334@gmail.com>
2024-10-25 08:56:26 +0800

    feat: linkedSignal in a service

--- src/app/services/shoe-sizes.service.ts
+++ src/app/services/shoe-sizes.service.ts
@@ -1,9 +1,53 @@
-import { Injectable } from '@angular/core';
+import { Injectable, linkedSignal, signal } from '@angular/core';
+
+const SHOE_SIZES = [5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10];
+const SHOE_SIZES2 = [4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoeSizesService {
 
-  constructor() { }
+  #shoeSizes = signal(SHOE_SIZES);
+  shoeSizes = this.#shoeSizes.asReadonly();
+
+  #currentShoeSize = linkedSignal<number[], number>({
+    source: this.#shoeSizes,
+    computation: (options, previous) => { 
+      if (!previous) {
+        // reset to the first size
+        return options[0];        
+      }
+
+      // shoe size found in the new shoe size list
+      if (options.includes(previous.value)) {
+        return previous.value;
+      }
+
+      // reset to the first shoe size because the new list does not contain the previous shoe size
+      return options[0]; 
+    }
+  });
+
+  currentShoeSize = this.#currentShoeSize.asReadonly();
+
+  updateShoeSize(value: number) {
+    console.log('updateShoeSize called', value);
+    this.#currentShoeSize.set(value);
+  }
+
+  changeShoeSizes() {
+    if (this.#shoeSizes()[0] === SHOE_SIZES2[0]) {
+      this.#shoeSizes.set(SHOE_SIZES);
+    } else {
+      this.#shoeSizes.set(SHOE_SIZES2);
+    }
+  }
+
+  updateLargestSize() {
+    const largestSize = this.#shoeSizes().at(-1);
+    if (typeof largestSize !== 'undefined') {
+      this.updateShoeSize(largestSize);
+    }
+  }
 }
--- src/app/shoe-sizes-store/shoe-sizes-store.component.ts
+++ src/app/shoe-sizes-store/shoe-sizes-store.component.ts
@@ -1,17 +1,53 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ShoeSizesService } from '../services/shoe-sizes.service';
 
 @Component({
   selector: 'app-shoe-sizes-store',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   template: `
-    <p>
-      shoe-sizes-store works!
-    </p>
+    <h2>Reset linked signal after updating source</h2>
+    <p>Source: {{ shoeSizes() }}</p>
+    <p>Shoe size: {{ currentShoeSize() }}</p>
+    <p>Shoe index: {{ index() }}</p>
+    <div>
+      <button (click)="changeShoeSizes()">Update shoe size source</button>
+      <button (click)="updateLargestSize()">Set to the largest size</button>
+    </div>
+    <label for="shoeSize">
+      <span>Choose a shoe size: </span>
+      <select id="shoeSize" name="shoeSize" [ngModel]="currentShoeSize()" (ngModelChange)="updateShoeSize($event)">
+        @for (size of shoeSizes(); track size) {
+          <option [ngValue]="size">{{ size }}</option>
+        }
+      </select>
+    </label>
   `,
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class ShoeSizesStoreComponent {
+  shoeSizesService = inject(ShoeSizesService);
+  
+  currentShoeSize = this.shoeSizesService.currentShoeSize;
+  shoeSizes = this.shoeSizesService.shoeSizes;
 
+  index = computed(() => this.shoeSizes().indexOf(this.currentShoeSize()));
+
+  constructor() {
+    this.updateShoeSize(5);
+  }
+
+  updateShoeSize(value: number) {
+    this.shoeSizesService.updateShoeSize(value);
+  }
+
+  changeShoeSizes() {
+    this.shoeSizesService.changeShoeSizes();
+  }
+
+  updateLargestSize() {
+    this.shoeSizesService.updateLargestSize();
+  }
 }
